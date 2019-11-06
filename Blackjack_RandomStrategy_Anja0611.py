c72fa7fef2527063b2c686b6ea6a1675fb66372e
Anja Koller <anjakoller@bluewin.ch>
2019-11-06 15:21:14 +0100

    add randomstrategy

---
+++ Blackjack_RandomStrategy_Anja0611.py
@@ -0,0 +1,45 @@
+import gym
+import matplotlib.pyplot as plt
+import timeit
+num_rounds = 100  # Payout calculated over num_rounds
+num_samples = 100  # num_rounds simulated over num_samples
+
+average_payouts = []
+
+
+def run_random():
+    for sample in range(num_samples):
+        round = 1
+        total_payout = 0  # to store total payout over 'num_rounds'
+
+    while round <= num_rounds:
+        action = env.action_space.sample()  # take random action
+        #         print('ACTION: ' + str(action))
+
+        obs, payout, is_done, info = env.step(action)
+        #         print('OBS: ' + str(obs))
+        #         print('PAYOUT: ' + str(payout))
+        #         print('ID_DONE: ' + str(is_done))
+        #         print('INFO: ' + str(info))
+
+        total_payout += payout
+        if is_done:
+            env.reset()  # Environment deals new cards to player and dealer
+            round += 1
+    average_payouts.append(total_payout)
+
+
+plt.plot(average_payouts)
+plt.xlabel('num_samples')
+plt.ylabel('payout after 1000 rounds')
+plt.show()
+print("Average payout after {} rounds is {}".format(num_rounds, sum(average_payouts) / num_samples))
+
+if __name__ == "__main__":
+    start = timeit.timeit()
+    env = gym.make('Blackjack-v0')
+    env.seed(0)
+    env.reset()
+    run_random()
+    end = timeit.timeit()
+    print(end - start)
