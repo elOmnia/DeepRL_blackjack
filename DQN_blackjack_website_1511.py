3b131cf171acff7a93ba4ad8af847e71acfbe89a
Anja Koller <anjakoller@bluewin.ch>
2019-11-29 18:37:06 +0100

    run strategy

--- DQN_blackjack_website_1511.py
+++ DQN_blackjack_website_1511.py
@@ -13,6 +13,7 @@ env = gym.make('Blackjack-v0')
 env.seed(0)
 start_time = time.time()
 
+
 class DQNAgent():
     def __init__(self, env, epsilon=1.0, alpha=0.5, gamma=0.9, time=30000):
         self.env = env
@@ -41,6 +42,7 @@ class DQNAgent():
         model.compile(loss='binary_crossentropy', optimizer=Adam(lr=self.alpha))
 
         return model
+
     #     # Remember function that stores states, actions, rewards, and done to memory
     #     def remember(self, state, action, reward, next_state, done):
     #         self.memory.append([state, action, reward, next_state, done])
@@ -85,6 +87,7 @@ class DQNAgent():
         target_f[0][action] = target
         self.model.fit(state, target_f, epochs=1, verbose=0)
 
+
     def get_optimal_strategy(self):
         index = []
         for x in range(0, 21):
@@ -99,13 +102,69 @@ class DQNAgent():
             df.loc[ind, 'Hit'] = outcome[0][1]
 
         df['Optimal'] = df.apply(lambda x: 'Hit' if x['Hit'] >= x['Stand'] else 'Stand', axis=1)
-        df.to_csv('optimal_policy.csv')
+        # TODO: give this another name for different parameters (mention nmber of rounds, etc)
+        df.to_csv('optimal_policy_100rounds_50samples_epsilon_0.1.csv')
         return df
+
+    def play_optimal_strategy(self, optimal_strategy):
+        """ for every state we are in choose the corresponding action to take according to the optimal strategy
+        that was computed and print out reward"""
+        print('RESET FROM BEGINNING')
+        done = False
+        state = env.reset()  # starts in state (12,10,False)
+        print('STATE OUTSIDE', state)
+        if(state[0]>=21):
+            done = True
+        print('DONE',done)
+        state = np.reshape(state[0:2], [1, 2])
+        state = state[0][0], state[0][1]
+        print('reshaped state',state)
+        #print('action at pos state', optimal_strategy['Optimal'][state])
+        # TODO: reshape the state in the correct index format and
+        # then for that state access the index Optimal column and read value out --> it value is Hit then action = 1
+        # else if value is stand then action is 0
+
+        while not done:
+            print('GO INTO DONE')
+            if(optimal_strategy['Optimal'][state] == 'Hit' or optimal_strategy['Optimal'][state] == 'Stand'):
+                #TODO: read out if there is hit or stand on pos index and change to while not done
+                print('action before', optimal_strategy['Optimal'][state])
+                action = 1 if optimal_strategy['Optimal'][state] == 'Hit' else 0
+                #action = 1 if optimal_strategy['Optimal'][state[0][0], state[0][1]] == 'Hit' else 0
+                print('action', action)
+                state, reward, done, _ = env.step(action);
+                if(state[0] >=21):
+                    print('21 or higher')
+                    break
+                print('stateinPlay',state)
+                state = np.reshape(state[0:2], [1, 2])
+                state = state[0][0], state[0][1]
+                print('reshaped state in play', state)
+                print('index', state)
+                print('reward at index', reward)
+            else:
+                action = random.randint(0,1)
+                print('random action',action)
+                state, reward, done, _ = env.step(action);
+                print('stateinPlay', state)
+                state = np.reshape(state[0:2], [1, 2])
+                state = state[0][0], state[0][1]
+                print('reshaped state in play', state)
+                print('index', state)
+                print('reward at index', reward)
+
+        if done:
+           print('GO INTO NOT DONE')
+           print('reward', reward)
+           state= env.reset()
+           print('resetted state',state)
+        return
+
 
 if __name__ == "__main__":
     num_rounds = 100  # Payout calculated over num_rounds
     num_samples = 50  # num_rounds simulated over num_samples
-    agent = DQNAgent(env=env, epsilon=1.0, alpha=0.001, gamma=0.1, time=7500)
+    agent = DQNAgent(env=env, epsilon=0.1, alpha=0.001, gamma=0.1, time=7500)
     average_payouts = []
     state = env.reset()
     state = np.reshape(state[0:2], [1, 2])
@@ -116,11 +175,9 @@ if __name__ == "__main__":
             action = agent.choose_action(state)
             next_state, payout, done, _ = env.step(action)
             next_state = np.reshape(next_state[0:2], [1, 2])
-
             total_payout += payout
-            #         if agent.learning:
+            #  learning phase
             agent.learn(state, action, payout, next_state, done)
-
             state = next_state
             state = np.reshape(state[0:2], [1, 2])
 
@@ -131,17 +188,15 @@ if __name__ == "__main__":
 
         average_payouts.append(total_payout)
 
-        if sample % 10 == 0:
-            print('Done with sample: ' + str(sample) + str("   --- %s seconds ---" % (time.time() - start_time)))
-            print(agent.epsilon)
+        #if sample % 10 == 0:
+        #    print('Done with sample: ' + str(sample) + str("   --- %s seconds ---" % (time.time() - start_time)))
+        #    print(agent.epsilon)
 
-    print(agent.get_optimal_strategy())
-
-# Plot payout per 1000 episodes for each value of 'sample'
+        optimal_strategy = agent.get_optimal_strategy();
+        optimal_strategy.to_csv('optimal_strategy_test.csv')
+        agent.play_optimal_strategy(optimal_strategy)
 
 plt.plot(average_payouts)
 plt.xlabel('num_samples')
 plt.ylabel('payout after 1000 rounds')
 plt.show()
-
-print("Average payout after {} rounds is {}".format(num_rounds, sum(average_payouts) / (num_samples)))
